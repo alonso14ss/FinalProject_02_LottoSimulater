6a16a5a8d072b60c59559202c019c28c4a558b1e
alonso14ss <reiyan92@gmail.com>
2020-03-24 11:35:58 +0900

    등수별 카운팅

--- app/src/main/java/co/kr/tjoeun/helloworld/finalproject_02_lottosimulater/MainActivity.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/finalproject_02_lottosimulater/MainActivity.java
@@ -27,6 +27,14 @@ public class MainActivity extends BaseActivity {
     long userMoneyAmount = 0;
     long winMoneyAmount = 0;
 
+    int firstRankCount = 0;
+    int secondRankCount = 0;
+    int thirdRankCount = 0;
+    int fourthRankCount = 0;
+    int fifthRankCount = 0;
+    int noRankCount = 0;
+
+
     ActivityMainBinding binding = null;
 
     @Override
@@ -171,6 +179,7 @@ public class MainActivity extends BaseActivity {
         if (correctCount == 6) {
 
             winMoneyAmount += 1200000000;
+            firstRankCount++;
 
         } else if (correctCount == 5) {
 //            2등인지 3등인지 재검사 필요= > 보너스 번호를 맞췄는가
@@ -188,24 +197,38 @@ public class MainActivity extends BaseActivity {
             if(hasBonusNum){
 //                2등
                 winMoneyAmount += 75000000;
+                secondRankCount++;
             }else {
 //                3등
                 winMoneyAmount += 1500000;
+                thirdRankCount++;
             }
 
 
         } else if (correctCount == 4) {
 
             winMoneyAmount += 50000;
+            fourthRankCount++;
 
         } else if (correctCount ==3) {
             userMoneyAmount -= 5000;
+            fifthRankCount++;
 
         }else {
+            noRankCount++;
 
         }
 
         binding.winMoneyTxt.setText(String.format("당첨 금액 : %d원",winMoneyAmount));
+
+//        당첨 카운팅 반영
+        binding.fisrtRankCountTxt.setText(String.format("1등 : %d회",firstRankCount));
+        binding.secondRankCountTxt.setText(String.format("2등 : %d회",secondRankCount));
+        binding.thirdRankCountTxt.setText(String.format("3등 : %d회",thirdRankCount));
+        binding.fourthRankCountTxt.setText(String.format("4등 : %d회",fourthRankCount));
+        binding.fifthRankCountTxt.setText(String.format("5등 : %d회",fifthRankCount));
+        binding.noRankCountTxt.setText(String.format("꼴등 : %d회",noRankCount));
+
 
     }
 }
